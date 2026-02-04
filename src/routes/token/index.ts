3a1d9d392e927d2bf7ae070680ecbbd306331083
Martin Lacko <martin.lacko@vacuumlabs.com>
2026-02-04 11:20:58 +0100

    Added DB configurable token tracking, with maintenance endpoints

--- src/routes/token/index.ts
+++ src/routes/token/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import priceRouter from './price';
 import holdersRouter from './holders';
 import volumeRouter from './volume';
+import manageRouter from './manage';
 
 const router = Router();
 
 router.use('/token/:tokenAddress/price', priceRouter);
 router.use('/token/:tokenAddress/holders', holdersRouter);
 router.use('/token/:tokenAddress/volume', volumeRouter);
+router.use('/', manageRouter);
 
 export default router;
